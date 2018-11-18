f3568c4feeb4162277139bedc51b5657d7e4773b
InnovaMinex <39905716+innovaminex@users.noreply.github.com>
2018-11-18 19:18:49 +0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Copyright (c) 2018 InnovaMinex
+// Copyright (c) 2018 http://cwd.systems
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
